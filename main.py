8972304ba204b63f19511177e8f15e779f4b37c6
po4an <43005263+po4an@users.noreply.github.com>
2021-07-22 00:31:43 +0300

    новые фичи

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import config
 import telebot
-from utils import db_execute
+from telebot import types
+from utils import db_execute, generate_markup, generate_img, delete_img, generate_inline
 import os
 import psycopg2 as pg
+import datetime
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from prettytable import from_db_cursor
 
 
 
 bot = telebot.TeleBot(config.token)
-
+expence_types = ["Табак", "HQD", "IZI", "Аксуссуары", "Личные расходы", "Зарплаты", "Для знакомых", "Другое"]
+common_count_rows = [3,5,10,15,30,50]
 
 
 """@bot.message_handler(commands = ["test"])
@@ -39,54 +44,135 @@ def hello(message):
 def info(message):
     bot.send_message(message.chat.id, "Команды, которые у меня есть:\n"
                                       "/info - узнать мои команды\n"
-                                      "/costs - внести расходы\n"
-                                      "/sum_costs - узнать сумму всех расходов")
+                                      "/add_expence - внести расходы\n"
+                                      "/sum_expences - узнать сумму всех расходов\n"
+                                      "/last5 - показать 5 последних расходов\n"
+                                      "/last_rows - показать определенное количество последних операций")
 
 """@bot.message_handler(content_types= ["text"])
 def ahah(message):
     bot.send_message(message.chat.id, message.text)"""
 
 
-@bot.message_handler(commands=['costs'])
-def to_incure_cost(message):
-    send = bot.send_message(message.chat.id, 'Внеси количество денег, сколько ты потратил')
-    bot.register_next_step_handler(send, to_incure_desc)
+@bot.message_handler(commands=['add_expence'])
+def add_expence(message):
+    send = bot.send_message(message.chat.id, 'Внеси количество денег, которые потратил', reply_markup=generate_inline([("отмена операции", "stop_operation")]))
+    bot.register_next_step_handler(send, add_desc)
 
 
-
-def to_incure_desc(message):
+def add_desc(message):
     global temp
     try:
         msg_val = int(message.text.lower())
         temp = [message.from_user.first_name + " " + message.from_user.last_name, msg_val]
-        send = bot.send_message(message.chat.id, 'Введи то, на что ты потратил деньги')
+        send = bot.send_message(message.chat.id, 'На что пошел расход?\nКраткое описание')
         print(temp)
-        bot.register_next_step_handler(send, costs_generate)
+        bot.register_next_step_handler(send, add_expence_type)
     except:
         send = bot.send_message(message.chat.id, 'Это не число, введи число')
-        bot.register_next_step_handler(send, to_incure_desc)
+        bot.register_next_step_handler(send, add_desc)
 
-def costs_generate(message):
+def add_expence_type(message):
+    global temp
+    temp.append(message.text.lower())
+    send = bot.send_message(message.chat.id, "У расхода есть какой то тип?", reply_markup=generate_markup(expence_types))
+    bot.register_next_step_handler(send, expence_generate)
+
+def expence_generate(message):
     global temp
     temp.append(message.text.lower())
     print(temp)
-    db_execute(f"insert into costs (user_name, cost, description) values ( '{temp[0]}', {temp[1]}, '{temp[2]}')")
-    bot.send_message(message.chat.id, "Успешно!\nТвоя запись в базе")
+    db_execute(f"insert into expences (user_name, expence, description, expence_type) values ( '{temp[0]}', {temp[1]}, '{temp[2]}', '{temp[3]}')")
+    bot.send_message(message.chat.id, "Успешно!\nТвоя запись в базе", reply_markup=types.ReplyKeyboardRemove())
 
 
-@bot.message_handler(commands=["sum_costs"])
-def sum_costs(message):
-    conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+@bot.message_handler(commands=["sum_expences"])
+def sum_expences_step1(message):
+    send = bot.send_message(message.chat.id, "За какую дату хотел бы посмотреть расходы?", reply_markup=generate_markup(["Текущий день", "Текущая неделя", "Текущий месяц", "Текущий год"]))
+    bot.register_next_step_handler(send, sum_expence_step2)
+
+def sum_expence_step2(message):
+    answer = message.text.lower()
+    if answer == "текущий день":
+        conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute("select sum(expence) from expences where load_dttm::date = current_date")
+                    sum_expences = cur.fetchone()[0]
+        finally:
+            conn.close()
+
+        bot.send_message(message.chat.id, f"Сумма трат за текущий день: {sum_expences}", reply_markup=types.ReplyKeyboardRemove())
+    if answer == "текущая неделя":
+        conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute("select sum(expence) from expences where extract(week from load_dttm) = extract(week from current_date) and extract(year from load_dttm) = extract(year from current_date)")
+                    sum_expences = cur.fetchone()[0]
+        finally:
+            conn.close()
+
+        bot.send_message(message.chat.id, f"Сумма трат за текущую неделю: {sum_expences}", reply_markup=types.ReplyKeyboardRemove())
+    if answer == "текущий месяц":
+        conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute("select sum(expence) from expences where extract(month from load_dttm) = extract(month from current_date) and extract(year from load_dttm) = extract(year from current_date)")
+                    sum_expences = cur.fetchone()[0]
+        finally:
+            conn.close()
+
+        bot.send_message(message.chat.id, f"Сумма трат за текущий месяц: {sum_expences}", reply_markup=types.ReplyKeyboardRemove())
+    if answer == "текущий год":
+        conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+        try:
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute("select sum(expence) from expences where extract(year from load_dttm) = extract(year from current_date)")
+                    sum_expences = cur.fetchone()[0]
+        finally:
+            conn.close()
+        bot.send_message(message.chat.id, f"Сумма трат за текущий год: {sum_expences}", reply_markup=types.ReplyKeyboardRemove())
+
+@bot.message_handler(commands=['last5'])
+def last(message):
+    generate_img(5)
+    with open('tmp.jpg', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo)
+    delete_img()
+
+@bot.message_handler(commands=['last_rows'])
+def count_rows(message):
+    markup = generate_markup(common_count_rows)
+    send = bot.send_message(message.chat.id, "Сколько последних операций хотели бы видеть?", reply_markup=markup)
+    bot.register_next_step_handler(send, fetch_rows)
+
+def fetch_rows(message):
+
     try:
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute("select sum(cost) from costs")
-                sum_costs = cur.fetchone()[0]
-    finally:
-        conn.close()
+        count = int(message.text.lower())
+        generate_img(count)
+        with open('tmp.jpg', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo, reply_markup=types.ReplyKeyboardRemove())
+        delete_img()
+    except:
+        send = bot.send_message(message.chat.id, "Мне нужно число")
+        bot.register_next_step_handler(send, fetch_rows)
 
-#добавить время добавления записи
-    send = bot.send_message(message.chat.id, f"Сумма трат : {sum_costs}")
+
+@bot.message_handler(commands=['test'])
+def inline(message):
+    markup = generate_inline()
+    bot.send_message(message.chat.id, 'kek', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == "stop_operation":
+        bot.clear_step_handler_by_chat_id(chat_id=call.message.chat.id)
+        bot.send_message(call.message.chat.id, "Операция отменена", reply_markup=types.ReplyKeyboardRemove())
 
 if __name__ == "__main__":
     bot.polling(none_stop = True)
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 import psycopg2 as pg
+from telebot import types
+import datetime
+import calendar
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from prettytable import from_db_cursor
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 def db_execute(command):
     conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
@@ -8,3 +15,52 @@ def db_execute(command):
                 cur.execute(command)
     finally:
         conn.close()
+
+
+def generate_markup(list):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    # Заполняем разметку перемешанными элементами
+    if len(list) % 4 == 0:
+        for i in range(0, len(list), 4):
+            markup.add(str(list[i]), str(list[i+1]), str(list[i+2]), str(list[i+3]))
+    elif len(list) % 3 == 0:
+        for i in range(0, len(list), 3):
+            markup.add(str(list[i]), str(list[i+1]), str(list[i+2]))
+    elif len(list) % 2 == 0:
+        for i in range(0, len(list), 2):
+            markup.add(str(list[i]), str(list[i+1]))
+    else:
+        for button in list:
+            markup.add(button)
+    return markup
+
+def generate_inline(list):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    for item in list:
+        but = types.InlineKeyboardButton(text = item[0], callback_data=item[1])
+        markup.add(but)
+    return markup
+
+def generate_img(num):
+    conn = pg.connect(host='localhost', dbname='di_bot', user='di_bot', password='dipadissdiwd', port='6543')
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(f"select * from expences order by load_dttm desc limit {num}")
+                mytable = from_db_cursor(cur)
+    finally:
+        conn.close()
+
+    out = Image.new("RGB", (1900, (110 + 28*num)), color='#272727')
+
+    d = ImageDraw.Draw(out)
+    font = ImageFont.truetype("consola.ttf", 32, encoding='UTF-8')
+
+    d.multiline_text((0,0), text=str(mytable), font=font, fill='#ffffff', align='center')
+    out.save("tmp.jpg")
+
+def delete_img():
+    os.remove("tmp.jpg")
+
+
+
